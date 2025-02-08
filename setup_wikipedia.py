9770f1cc6d26847224d36397b5d1934ecf25cc37
johng149 <johnguo149@gmail.com>
2025-02-08 04:26:23 +0000

    Add setup script for wikipedia

---
+++ setup_wikipedia.py
@@ -0,0 +1,13 @@
+from src.datasets.wikipedia.dataset import process, Dataset
+from transformers import AutoTokenizer
+from pathlib import Path
+
+def setup():
+    dataset_path = "data/wikipedia"
+    train_path = Path(dataset_path) / "train"
+    if not train_path.exists():
+        tokenizer = AutoTokenizer.from_pretrained("gpt2")
+        process(tokenizer, "gpt2", dataset_path)
+
+if __name__ == "__main__":
+    setup()
